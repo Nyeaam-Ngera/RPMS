4ff892a44733856206cb7f7d61e074c9f31f14d0
kelvezu <44080252+kelvezu@users.noreply.github.com>
2019-08-25 04:55:56 +0800

    Update processtrubric.php
    added the delete process

--- includes/processtrubric.php
+++ includes/processtrubric.php
@@ -1,8 +1,4 @@
 <?php
-
-
-
-
 $mysqli = new mysqli('localhost', 'root', '' ,'rpms') or die(mysqli_error($mysqli));
 
 if(isset($_POST['save'])){
@@ -10,13 +6,15 @@ if(isset($_POST['save'])){
     $level_name = $_POST['level_name'];
     $rubric_description = $_POST['rubric_description'];
 
-
-    $mysqli->query("INSERT INTO trubric_tbl(rubric_lvl,level_name,rubric_description) VALUES ('$rubric_lvl','$level_name','$rubric_description')") or die($mysqli->error);
-
-    
+    $mysqli->query("INSERT INTO trubric_tbl(rubric_lvl,level_name,rubric_description) VALUES ('$rubric_lvl','$level_name','$rubric_description')") or die($mysqli->error);  
     header('location:../tRubric.php?notif=added');
     echo '<script>window.alert("Rubric Level Added!")</script>'; 
+}
 
+if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    $mysqli->query("DELETE FROM trubric_tbl WHERE rubric_id=$id") or die($mysqli->error);
+    header('location:../displaytRubric.php?notif=deleted');
 }
 
 ?>
