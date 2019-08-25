9153ce3ae5eb72965aeddcc59cbec37ebe6d75fa
kelvezu <44080252+kelvezu@users.noreply.github.com>
2019-08-26 05:39:11 +0800

    Update processtrubric.php

--- includes/processtrubric.php
+++ includes/processtrubric.php
@@ -1,39 +1,69 @@
-<?php
+<header>
+<?php include 'header.php'; 
+    if(isset($_GET['notif'])){
+        if($_GET['notif'] == "added"){
+            echo '<script class="alert-info">window.alert("Rubric Added") </script>';
+        } elseif($_GET['notif'] == "deleted"){
+            echo '<script class="alert-danger">window.alert("Rubric Deleted") </script>';
+        }
+        else{
+            echo "";
+        }
+    }
 
-$rubric_lvl = $level_name = $rubric_description = "";
-$rubric_id = 0;
+    if(isset($_GET['edit'])){
+        $rubric_id = $_GET['edit'];
+        
+        $query = mysqli_query($conn,"SELECT * FROM trubric_tbl WHERE rubric_id=$rubric_id");
+        $record = mysqli_fetch_array($query);
+        $rubric_lvl = $record['rubric_lvl'];
+        $level_name = $record['level_name'];
+        $rubric_description = $record['rubric_description'];
 
-$conn = new mysqli('localhost', 'root', '' ,'rpms') or die(mysqli_error($conn));
-
-if(isset($_POST['save'])){
-    $rubric_lvl  = $_POST['rubric_lvl'];
-    $level_name = $_POST['level_name'];
-    $rubric_description = $_POST['rubric_description'];
-
-
-    $conn->query("INSERT INTO trubric_tbl(rubric_lvl,level_name,rubric_description) VALUES ('$rubric_lvl','$level_name','$rubric_description')") or die($conn->error);
-
-
-    header('location:../displaytRubric.php?notif=added');
+    }
     
-}
-
-if(isset($_GET['delete'])){
-    $id = $_GET['delete'];
-    $conn->query("DELETE FROM trubric_tbl WHERE rubric_id=$id") or die($conn->error);
-    header('location:../displaytRubric.php?notif=deleted');
-}
-
-if(isset($_POST['update'])){
-    $rubric_id = $_POST['rubric_id'];
-    $rubric_lvl = $_POST['rubric_lvl'];
-    $level_name = $_POST['level_name'];
-    $rubric_description = $_POST['rubric_description'];
-
-    mysqli_query($conn,"UPDATE trubric_tbl SET rubric_lvl = '$rubric_lvl', level_name = '$level_name', rubric_description = '$rubric_description' WHERE rubric_id = '$rubric_id' ");
-    header("location:../displaytRubric.php?notif=updated");
-
-}
-
 ?>
+
+</header>
+
+<main>
+    
   
+        <div class="container col-sm-6 my-4 bg-light ">
+        <form action="includes/processtrubric.php" class="form-group sm" method="POST">
+       
+                <legend class="legend-control bg-success text-white ">Rubric Level Summary for Teacher I-III</legend>
+                    <div class="row">
+                        <div class="col-sm-6">
+                        <input type="hidden" name="rubric_id" value="<?php echo $rubric_id; ?>"/>
+                            <label for="level" class="control-label"><strong>Rubric Level</strong></label>
+                            <input type="number" name="rubric_lvl" id="rubric-lvl" class="form-control" width="500" value="<?php echo $rubric_lvl; ?>"placeholder="Enter the rubric level..."> 
+                        </div>
+
+                        <div class="col-sm-6">
+                            <label for="level-name" class="control-label"><strong>Level Name</strong></label>
+                            <input type="text" name="level_name" id=level-name" class="form-control" width="500" value="<?php echo $level_name; ?>" placeholder="Enter the Level Name..."/> 
+                        </div>
+                    </div>
+                    
+                    <div>
+                            <label for="description" class="control-label w-25 "><strong>Description</strong></label>
+                            <textarea name="rubric_description" id="policy-content" cols="5" rows="5" class="form-control" value="" placeholder="Enter the description..."><?php echo $rubric_description ?></textarea>
+                        </div>  
+                        <div class="row">
+                        
+                            
+                            <button type="submit" class="btn btn-primary my-4" name="update">Update</button> &nbsp
+                             <a  class="btn btn-danger my-4" href="displaytRubric.php" role="button">Cancel</a>
+                            
+                        </div>
+        </form>
+       
+        </div>
+    
+
+</main>
+
+<footer>
+
+</footer>
