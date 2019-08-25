e593a2959a639035e94b2c0343d39351b4bccff3
kelvezu <44080252+kelvezu@users.noreply.github.com>
2019-08-26 01:52:37 +0800

    Add files via upload
    added the update function

--- includes/processtrubric.php
+++ includes/processtrubric.php
@@ -1,4 +1,13 @@
 <?php
+
+$rubric_lvl = $level_name = $rubric_description = "";
+$edit_state = false;
+$rubric_id = 0;
+
+
+
+
+
 $mysqli = new mysqli('localhost', 'root', '' ,'rpms') or die(mysqli_error($mysqli));
 
 if(isset($_POST['save'])){
@@ -6,9 +15,12 @@ if(isset($_POST['save'])){
     $level_name = $_POST['level_name'];
     $rubric_description = $_POST['rubric_description'];
 
-    $mysqli->query("INSERT INTO trubric_tbl(rubric_lvl,level_name,rubric_description) VALUES ('$rubric_lvl','$level_name','$rubric_description')") or die($mysqli->error);  
-    header('location:../tRubric.php?notif=added');
-    echo '<script>window.alert("Rubric Level Added!")</script>'; 
+
+    $mysqli->query("INSERT INTO trubric_tbl(rubric_lvl,level_name,rubric_description) VALUES ('$rubric_lvl','$level_name','$rubric_description')") or die($mysqli->error);
+
+
+    header('location:../displaytRubric.php?notif=added');
+    
 }
 
 if(isset($_GET['delete'])){
@@ -17,4 +29,16 @@ if(isset($_GET['delete'])){
     header('location:../displaytRubric.php?notif=deleted');
 }
 
+if(isset($_POST['update'])){
+    $rubric_id = $_POST['rubric_id'];
+    $rubric_lvl = $_POST['rubric_lvl'];
+    $level_name = $_POST['level_name'];
+    $rubric_description = $_POST['rubric_description'];
+
+    mysqli_query($mysqli,"UPDATE trubric_tbl SET rubric_lvl = '$rubric_lvl', level_name = '$level_name', rubric_description = '$rubric_description' WHERE rubric_id = '$rubric_id' ");
+    header("location:../displaytRubric.php?notif=updated");
+
+}
+
 ?>
+  
