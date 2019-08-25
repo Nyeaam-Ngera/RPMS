7ad353542c5f50feb7f1491460a8260ed159a96d
kelvezu <44080252+kelvezu@users.noreply.github.com>
2019-08-26 02:39:00 +0800

    Update tRubric.php
    remove some unnecessary functions

--- tRubric.php
+++ tRubric.php
@@ -1,43 +1,47 @@
 <header>
 <?php include 'header.php'; 
-    if(isset($_GET['notif'])){
-        if($_GET['notif'] == "added"){
-            echo '<script class="alert-info">window.alert("Rubric Added") </script>';
-        }else{
-            echo "";
-        }
-    }
-    
+   
+
+
 ?>
 
 </header>
 
 <main>
-    <?php require_once 'includes/processtrubric.php'; ?>
+    
   
-        <div class="container col-sm-8 my-4 bg-light ">
+        <div class="container col-sm-6 my-4 bg-light ">
         <form action="includes/processtrubric.php" class="form-group sm" method="POST">
+       
                 <legend class="legend-control bg-success text-white ">Rubric Level Summary for Teacher</legend>
                     <div class="row">
                         <div class="col-sm-6">
+                        <input type="hidden" name="rubric_id" value="<?php echo $rubric_id; ?>"/>
                             <label for="level" class="control-label"><strong>Rubric Level</strong></label>
-                            <input type="number" name="rubric_lvl" id="" class="form-control" width="500"> 
+                            <input type="number" name="rubric_lvl" id="rubric-lvl" class="form-control" width="500" placeholder="Enter the rubric level..."> 
                         </div>
 
                         <div class="col-sm-6">
                             <label for="level-name" class="control-label"><strong>Level Name</strong></label>
-                            <input type="text" name="level_name" id=level-name" class="form-control" width="500"> 
+                            <input type="text" name="level_name" id=level-name" class="form-control" width="500"  placeholder="Enter the Level Name..."/> 
                         </div>
                     </div>
                     
                     <div>
                             <label for="description" class="control-label w-25 "><strong>Description</strong></label>
-                            <textarea name="rubric_description" id="policy-content" cols="5" rows="5" class="form-control"></textarea>
+                            <textarea name="rubric_description" id="policy-content" cols="5" rows="5" class="form-control" value="" placeholder="Enter the description..."></textarea>
                         </div>  
-                        <div>
+                        <div class="row">
+                            <div class="col-md-12">
                             <button type="submit" class="btn-sm btn-success my-4" name="save">Save</button>
+
+                            <a href="displaytRubric.php" class="btn-sm btn-primary my-4" role="button">Show Rubrics</a>
+                            </div>
+
+                            
                         </div>
         </form>
+       
         </div>
     
 
